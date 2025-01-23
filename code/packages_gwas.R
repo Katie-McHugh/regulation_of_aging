7a7e47d59436349723835aee0b4fcfdef92f0e69
Katie McHugh <mchughk@oregonstate.edu>
2025-01-22 19:11:34 -0800

    Load necessary packages for GWAS analysis

---
+++ code/packages_gwas.R
@@ -0,0 +1,15 @@
+## Install packages and load library for analysis of GWAS data
+
+#install.packages("data.table")    # Ensures version >= 1.9.4
+#install.packages("foreach")       # Ensures version >= 1.4.2
+#install.packages("stringi")       # Ensures version >= 0.4-1
+#install.packages("matrixStats")   # Ensures version >= 0.14.2
+#install.packages("Rcpp")  
+#install.packages("data_files/poolSeq-0.3.5.tar.gz", repos=NULL, type="source")
+library(geneSLOPE)
+library(ggplot2)
+library(dplyr)
+library(devtools)
+library(poolSeq) #for CMH test
+library(tidyr)
+library(stringr)
