bdfc7c9b23930eaf75a6d069d367028ebbc8311c
Katie McHugh <mchughk@oregonstate.edu>
2025-03-21 12:41:39 -0700

    Improve formatting and readability

--- code/comparisons/GO_term_plot.R
+++ code/comparisons/GO_term_plot.R
@@ -8,112 +8,100 @@
 
 # if (!require("BiocManager", quietly = TRUE))
 #   install.packages("BiocManager")
-# 
-# BiocManager::install("clusterProfiler")
-# 
+
 # BiocManager::install("pathview")
 # BiocManager::install("enrichplot")
-library(clusterProfiler)
-library(enrichplot)
-
-# browseVignettes("clusterProfiler")
-# BiocManager::install("topGO")
-# browseVignettes("topGO")
-
-BiocManager::install("org.Sc.sgd.db")
-
-# Install biomaRt package (if not already installed)
-BiocManager::install("biomaRt")
-
-# Load the biomaRt library
-library(biomaRt)
-
-# Install the necessary package
-BiocManager::install("org.Sc.eg.db")
-# Load the package
-library(org.Sc.eg.db)
-
-
-
-
-
-#------------------------------------------------------------------------------
-## Load in gene lists #make sure using only lists of genic variants
-#------------------------------------------------------------------------------
-
-genes_dna<-read.csv("temp/genome/genic_GO_list.txt")
-genes_rna<-read.csv("temp/transcriptome/DGEgene_list.txt")
-genes_both<-read.csv("temp/comparisons/genic_combined_GO_list.txt")
-
-#------------------------------------------------------------------------------
-## Convert Gene Lists to EntreZ IDs
-#------------------------------------------------------------------------------
-
-# Connect to the Ensembl BioMart database
-ensembl <- useMart("ENSEMBL_MART_ENSEMBL", dataset = "scerevisiae_gene_ensembl")
-
-# You can check the available attributes (fields) and filters (options) in the Mart
-attributes <- listAttributes(ensembl)
-list(attributes)
-filters <- listFilters(ensembl)
-View(filters)
-
-#-----------------------------------
-####### DNA list ###############
-#-----------------------------------
-results_dna <- getBM(
-  attributes = c("external_gene_name", "entrezgene_id"),  # Gene names and Entrez IDs
-  filters = "external_gene_name",                      # Use gene names as filter
-  values = genes_dna,                                 # Your list of gene names
-  mart = ensembl                                       # The Ensembl mart
-)
-
-attributes <- listAttributes(ensembl)
-head(attributes)
-# Print the results
-print(results_dna)
-
-View(attributes)
-
-dna_list<-results_dna$entrezgene_id
-
-write.table(dna_list, file="temp/genome/dna_entrezIDs.txt", 
-            row.names= FALSE, col.names= FALSE)
-
-#-----------------------------------
-####### RNA list ###################
-#-----------------------------------
-
-results_rna <- getBM(
-  attributes = c("external_gene_name", "entrezgene_id"),  # Gene names and Entrez IDs
-  filters = "external_gene_name",                      # Use gene names as filter
-  values = genes_rna,                                 # Your list of gene names
-  mart = ensembl                                       # The Ensembl mart
-)
-
-print(results_rna)
-
-rna_list<-results_rna$entrezgene_id
-
-write.table(rna_list, file="temp/transcriptome/rna_entrezIDs.txt", 
-            row.names= FALSE, col.names= FALSE)
-
-#-----------------------------------
-####### Combined list ##############
-#-----------------------------------
-
-results_both <- getBM(
-  attributes = c("external_gene_name", "entrezgene_id"),  
-  filters = "external_gene_name",                      
-  values = genes_both,                                 
-  mart = ensembl                                      
-)
-
-both_list<-results_both$entrezgene_id
-
-write.table(both_list, file="temp/comparisons/rna&dna_entrezIDs.txt", 
-            row.names= FALSE, col.names= FALSE)
-
+# #library(enrichplot)
+# 
+# #BiocManager::install("org.Sc.sgd.db")
+# 
+# # Install biomaRt package (if not already installed)
+# BiocManager::install("biomaRt")
+# 
+# # Load the biomaRt library
+# library(biomaRt)
+# 
+# # Install the necessary package
+# #BiocManager::install("org.Sc.eg.db")
+# # Load the package
+# #library(org.Sc.eg.db)
+# 
+# 
+# #------------------------------------------------------------------------------
+# ## Load in gene lists #make sure using only lists of genic variants
+# #------------------------------------------------------------------------------
+# 
+# genes_dna<-read.csv("temp/genome/genic_GO_list.txt")
+# genes_rna<-read.csv("temp/transcriptome/DGEgene_list.txt")
+# genes_both<-read.csv("temp/comparisons/genic_combined_GO_list.txt")
+# 
+# #------------------------------------------------------------------------------
+# ## Convert Gene Lists to EntreZ IDs
+# #------------------------------------------------------------------------------
+# 
+# # Connect to the Ensembl BioMart database
+# ensembl <- useMart("ENSEMBL_MART_ENSEMBL", dataset = "scerevisiae_gene_ensembl")
+# 
+# # You can check the available attributes (fields) and filters (options) in the Mart
+# attributes <- listAttributes(ensembl)
+# filters <- listFilters(ensembl)
+# 
+# #-----------------------------------
+# ####### DNA list ###############
+# #-----------------------------------
+# results_dna <- getBM(
+#   attributes = c("external_gene_name", "entrezgene_id"),  # Gene names and Entrez IDs
+#   filters = "external_gene_name",                      # Use gene names as filter
+#   values = genes_dna,                                 # Your list of gene names
+#   mart = ensembl                                       # The Ensembl mart
+# )
+# 
+# attributes <- listAttributes(ensembl)
+# head(attributes)
+# # Print the results
+# print(results_dna)
+# 
+# View(attributes)
+# 
+# dna_list<-results_dna$entrezgene_id
+# 
+# write.table(dna_list, file="temp/genome/dna_entrezIDs.txt", 
+#             row.names= FALSE, col.names= FALSE)
+# 
+# #-----------------------------------
+# ####### RNA list ###################
+# #-----------------------------------
+# 
+# results_rna <- getBM(
+#   attributes = c("external_gene_name", "entrezgene_id"),  # Gene names and Entrez IDs
+#   filters = "external_gene_name",                      # Use gene names as filter
+#   values = genes_rna,                                 # Your list of gene names
+#   mart = ensembl                                       # The Ensembl mart
+# )
+# 
+# print(results_rna)
+# 
+# rna_list<-results_rna$entrezgene_id
+# 
+# write.table(rna_list, file="temp/transcriptome/rna_entrezIDs.txt", 
+#             row.names= FALSE, col.names= FALSE)
+# 
+# #-----------------------------------
+# ####### Combined list ##############
+# #-----------------------------------
+# 
+# results_both <- getBM(
+#   attributes = c("external_gene_name", "entrezgene_id"),  
+#   filters = "external_gene_name",                      
+#   values = genes_both,                                 
+#   mart = ensembl                                      
+# )
+# 
+# both_list<-results_both$entrezgene_id
+# 
+# write.table(both_list, file="temp/comparisons/rna&dna_entrezIDs.txt", 
+#             row.names= FALSE, col.names= FALSE)
+# 
 
 #------------------------------------------------------------------------------
 # Try to load in GO-term data
@@ -218,13 +206,14 @@ go_dna$gene_list<-relevel(go_dna$gene_list, ref="DNA")
 plot3<-ggplot(go_dna, aes(x = reorder(TERM, -CORRECTED_PVALUE), y = CORRECTED_PVALUE, 
                           size = NUM_LIST_ANNOTATIONS, color = ann)) +
   geom_point(alpha = 1) +  
-  scale_color_manual(values = c("component" = "blue", "process" = "red", "function"="goldenrod4")) +  # Custom colors for Component and Function
+  scale_color_manual(name = "GO-Term \nCategory", values = c("component" = "blue", "process" = "red", "function"="goldenrod4")) +  # Custom colors for Component and Function
   labs(x = "GO Terms", y = "Corrected P-value", 
        title = "GO Term Enrichment") +
   theme_minimal() +
   coord_flip() +  # Flip coordinates to make the plot horizontal
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_size_continuous(name = "Number of Annotated Genes")  +
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 20)) +  # Wrap y-axis labels
+  scale_size_continuous(name = "Number of \nAnnotated \nGenes")  +
   facet_wrap(~gene_list, scales = "fixed")  + 
   theme_light() +
   theme(strip.text = element_text(size = 14, face = "bold"))
