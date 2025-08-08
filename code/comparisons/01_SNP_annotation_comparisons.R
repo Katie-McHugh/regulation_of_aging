303805109f791347662aa16412475594d230462b
Katie McHugh <mchughk@oregonstate.edu>
2025-08-08 13:50:01 -0600

    clean code

--- code/comparisons/01_SNP_annotation_comparisons.R
+++ code/comparisons/01_SNP_annotation_comparisons.R
@@ -23,8 +23,6 @@ ann_s<-read.table("data/raw/annotated_snps.txt", header=TRUE)  #snps only
 ### Now look at SNPeffs whole-genome annotations
 ##############################################################################
 
-################ edit this to look at SNPs and indels ########################
-
 ### Just look at SNPs - can change to be both
 ##ann<-rbind(ann_s, ann_i)
 ann<-ann_s
@@ -35,11 +33,29 @@ ann_f <- ann %>%
 
 #### Step 2: Separate the First_ANN column by pipe character into multiple columns
 ann_f <- ann_f %>%
-  separate(First_ANN, into = c("Allele", "Annotation", "Annotation_Impact", "Gene_Name", "Gene_ID", "Feature_Type", "Feature_ID", "Transcript_BioType", "Rank", "HGVS.c", "HGVS.p", "cDNA.pos / cDNA.length", "CDS.pos / CDS.length", "AA.pos / AA.length", "Distance", "INFO"), sep = "\\|", fill = "right")
+  separate(First_ANN, into = c("Allele", 
+                               "Annotation", 
+                               "Annotation_Impact", 
+                               "Gene_Name", 
+                               "Gene_ID", 
+                               "Feature_Type", 
+                               "Feature_ID", 
+                               "Transcript_BioType", 
+                               "Rank", 
+                               "HGVS.c", 
+                               "HGVS.p",
+                               "cDNA.pos / cDNA.length", 
+                               "CDS.pos / CDS.length", 
+                               "AA.pos / AA.length", 
+                               "Distance", 
+                               "INFO"), 
+           sep = "\\|", fill = "right")
 
 ### subset relevant information
 ann_f2<-ann_f[,-c(13:60)]
-ann_f3<-ann_f[c("CHROM", "POS", "TYPE", "Gene_ID", "Gene_Name", "Annotation","Annotation_Impact","Feature_Type", "Transcript_BioType", "EVENTLENGTH", "ANN")]
+ann_f3<-ann_f[c("CHROM", "POS", "TYPE", "Gene_ID", "Gene_Name", "Annotation",
+                "Annotation_Impact","Feature_Type", "Transcript_BioType", 
+                "EVENTLENGTH", "ANN")]
 
 
 ### first annotation for every annotated variant in the genome
