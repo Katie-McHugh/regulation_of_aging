98eaaaedf1b3033c9721423436317f915697bfef
Katie McHugh <mchughk@oregonstate.edu>
2026-02-18 12:29:03 -0700

    reorganizing results files

--- code/transcriptome/02_DGE_gene_lists.R
+++ code/transcriptome/02_DGE_gene_lists.R
@@ -3,7 +3,8 @@
 #------------------------------------------------------------------------------
 
 ### Load results object
-res_adj<-read.csv("temp/transcriptome/rnaseq_results_batch_adjusted.csv", header=TRUE)
+res_adj<-read.csv("data/clean/rnaseq_results_batch_adjusted.csv", header=TRUE)
+
 head(res_adj)
 # find genes below p<0.05
 sig_genes_adj <- res_adj[which(res_adj$padj <= 0.05), ] #27 genes 
